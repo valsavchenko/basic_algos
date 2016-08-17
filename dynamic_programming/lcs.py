c4eae0d090ddbf5386229d9d0f585f42af7757d4
Valentine Savchenko <valentine.savchenko@gmail.com>
2016-08-17 20:19:14 +0300

    (1) Reducing space required for the algorithm by eliminating the back tracking matrix b (2) Backtracking algorithm now has increased buried constant

--- dynamic_programming/lcs.py
+++ dynamic_programming/lcs.py
@@ -11,40 +11,32 @@ a subsequence of both X and Y .
 The longest-common-subsequence problem. Given two sequences X = <x1, x2, ..., xm> 
 and Y = <y1, y2, ..., yn>, find a maximum length common subsequence of X and Y.
 """
-
 def lcs(X, Y):
   m = len(X)
   n = len(Y)
   c = [[0 for j in range(n+1)] for i in range(m+1)]
-  b = [[(0, 0) for j in range(n)] for i in range(m)]
-
-  WN = (-1, -1)
-  N = (-1, 0)
-  W = (0, -1)
 
   for i in range(1, m+1):
     for j in range(1, n+1):
       if X[i-1] == Y[j-1]:
         c[i][j] = c[i-1][j-1] + 1
-        b[i-1][j-1] = WN
       elif c[i-1][j] >= c[i][j-1]:
         c[i][j] = c[i-1][j]
-        b[i-1][j-1] = N
       else:
         c[i][j] = c[i][j-1]
-        b[i-1][j-1] = W
 
-  i = m-1
-  j = n-1
+  i = m
+  j = n
   seq = []
-  while i >= 0 and j >= 0:
-    if X[i] == Y[j]:
-      seq.insert(0, X[i])
-    ii = i
-    jj = j
-    i += b[ii][jj][0]
-    j += b[ii][jj][1]
-    assert(ii != i or jj != j)
+  while i > 0 and j > 0:
+    if X[i-1] == Y[j-1]:
+      seq.insert(0, X[i-1])
+      i = i-1
+      j = j-1
+    elif c[i][j] == c[i-1][j]:
+      i = i-1
+    else:
+      j = j-1
 
   return seq
 
@@ -53,6 +45,7 @@ class Tests(unittest.TestCase):
     self.assertEqual(lcs(list('ACCGGTCGAGTGCGCGGAAGCCGGCCGAA'),
                          list('GTCGTTCGGAATGCCGTTGCTCTGTAAA')),
                          list('GTCGTCGGAAGCCGGCCGAA'))
+
   def test_motivational_lcs_example(self):
     self.assertEqual(lcs(list('ABCBDAB'), list('BDCABA')), list('BCBA'))
 
