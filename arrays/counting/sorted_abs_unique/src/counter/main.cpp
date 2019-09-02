e63e3e0aaeed08977a919a5eb788ddb881b808c8
Valentine Savchenko <valentine.savchenko@gmail.com>
2019-09-03 00:16:42 +0300

    Supply an O(N) runtime / O(1) space solution
    Provide a bunch of unit tests for the solution. Beautify the standalone
    executable to challenge the solution through the command line.

--- arrays/counting/sorted_abs_unique/src/counter/main.cpp
+++ arrays/counting/sorted_abs_unique/src/counter/main.cpp
@@ -1,8 +1,38 @@
-#include <iostream>
-
 #include "Counter.h"
 
-int main(int argc, const char **argp)
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <iostream>
+
+using namespace counter;
+
+int main(int argc, const char** argv)
 {
-  return 0;
+    NonDecreasingIntegers values{};
+    values.reserve(argc - 1);
+
+    try
+    {
+        std::transform(argv + 1, argv + argc, std::back_inserter<NonDecreasingIntegers>(values),
+            [](const char* arg)
+            {
+                return std::stol(arg);
+            });
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "The elements are not integers!\n";
+        return 1;
+    }
+
+    if (!std::is_sorted(values.cbegin(), values.cend()))
+    {
+        std::cout << "The elements are not ordered!\n";
+        return 2;
+    }
+
+    const auto count = count_unique_absolute_values(values);
+    std::cout << "Number of unique elements is " << count << "\n";
+    return 0;
 }
--- arrays/counting/sorted_abs_unique/test/counter_test.cpp
+++ arrays/counting/sorted_abs_unique/test/counter_test.cpp
@@ -6,18 +6,122 @@ using namespace counter;
 
 TEST(Sample, Case1)
 {
-    const auto count = count_unique_abs_values({-11, -7, -5, -1, 0, 1, 3});
+    const auto count = count_unique_absolute_values({ -11, -7, -5, -1, 0, 1, 3 });
     EXPECT_EQ(6, count);
 }
 
 TEST(Sample, Case2)
 {
-    const auto count = count_unique_abs_values({-11, 11});
+    const auto count = count_unique_absolute_values({ -11, 11 });
     EXPECT_EQ(1, count);
 }
 
 TEST(Sample, Case3)
 {
-    const auto count = count_unique_abs_values({-11, -7, 11, 11});
+    const auto count = count_unique_absolute_values({ -11, -7, 11, 11 });
     EXPECT_EQ(2, count);
+}
+
+TEST(Tiny, Empty)
+{
+    const auto count = count_unique_absolute_values({});
+    EXPECT_EQ(0, count);
+}
+
+TEST(Tiny, LonePositive)
+{
+    const auto count = count_unique_absolute_values({ 1 });
+    EXPECT_EQ(1, count);
+}
+
+TEST(Tiny, LoneZero)
+{
+    const auto count = count_unique_absolute_values({ 0 });
+    EXPECT_EQ(1, count);
+}
+
+TEST(Tiny, LoneNegative)
+{
+    const auto count = count_unique_absolute_values({ -1 });
+    EXPECT_EQ(1, count);
+}
+
+TEST(Homogeneous, UniquePositives)
+{
+    const NonDecreasingIntegers values{ 0, 8, 13, 62, 7292 };
+    const auto count = count_unique_absolute_values(values);
+    EXPECT_EQ(values.size(), count);
+}
+
+TEST(Homogeneous, DuplicatesAtHeadPositives)
+{
+    const auto count = count_unique_absolute_values({0, 0, 0, 1, 2, 3, 4});
+    EXPECT_EQ(5, count);
+}
+
+TEST(Homogeneous, DuplicatesInMiddlePositives)
+{
+    const auto count = count_unique_absolute_values({0, 1, 2, 2, 2, 2, 2, 3, 4, 5});
+    EXPECT_EQ(6, count);
+}
+
+TEST(Homogeneous, DuplicatesAtTailPositives)
+{
+    const auto count = count_unique_absolute_values({0, 1, 2, 3, 3});
+    EXPECT_EQ(4, count);
+}
+
+TEST(Homogeneous, TotallyDuplicatePositives)
+{
+    const auto count = count_unique_absolute_values({ 77, 77, 77, 77, 77, 77 });
+    EXPECT_EQ(1, count);
+}
+
+TEST(Homogeneous, UniqueNegatives)
+{
+    const NonDecreasingIntegers values{ -62, -13, -8 };
+    const auto count = count_unique_absolute_values(values);
+    EXPECT_EQ(values.size(), count);
+}
+
+TEST(Homogeneous, DuplicatesAtHeadNegatives)
+{
+    const auto count = count_unique_absolute_values({ -72, -72, -72, -55, -44, -4, -1 });
+    EXPECT_EQ(5, count);
+}
+
+TEST(Homogeneous, DuplicatesInMiddleNegatives)
+{
+    const auto count = count_unique_absolute_values({ -7, -6, -5, -5, -5, -4, -3, -1 });
+    EXPECT_EQ(6, count);
+}
+
+TEST(Homogeneous, DuplicatesAtTailNegatives)
+{
+    const auto count = count_unique_absolute_values({ -11, -7, -5, -2, -2, -2, -2 });
+    EXPECT_EQ(4, count);
+}
+
+TEST(Homogeneous, TotallyDuplicateNegatives)
+{
+    const auto count = count_unique_absolute_values({ -13, -13 });
+    EXPECT_EQ(1, count);
+}
+
+TEST(Assorted, DuplicateHalves)
+{
+    const auto count = count_unique_absolute_values({-10, -10, -10, 0, 0, 0, 0});
+    EXPECT_EQ(2, count);
+}
+
+TEST(Assorted, MirroredIdenticalHalves)
+{
+    const auto count = count_unique_absolute_values({ -7, -7, -7, -7, 7, 7 });
+    EXPECT_EQ(1, count);
+}
+
+TEST(Assorted, MirroredDuplicateHalves)
+{
+    const auto count = count_unique_absolute_values({-3, -3, -3, -2, -2, -1, 1, 1, 1, 2, 2, 3});
+    EXPECT_EQ(3, count);
 }
