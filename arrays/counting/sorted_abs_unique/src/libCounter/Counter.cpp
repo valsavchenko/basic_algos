3acf069d162984aa5f6b0bea68f250cbf5fd6c5e
Valentine Savchenko <valentine.savchenko@gmail.com>
2019-09-03 00:48:13 +0300

    Eliminate an accident copy-paste

--- arrays/counting/sorted_abs_unique/src/libCounter/Counter.cpp
+++ arrays/counting/sorted_abs_unique/src/libCounter/Counter.cpp
@@ -3,14 +3,9 @@
 
 #include <cassert>
 #include <algorithm>
-#include <vector>
 
 namespace counter
 {
-
-using Count = unsigned long long;
-using Integer = long long;
-using NonDecreasingIntegers = std::vector<Integer>;
 
 Count count_unique_absolute_values(const NonDecreasingIntegers& values) noexcept
 {
